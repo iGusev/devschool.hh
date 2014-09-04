999620f67fc419b81cddc3c22f259b046c8d8b06
Ilya <mail@igusev.ru>
2014-09-04 19:08:56 +0400

    task #2

---
+++ task2.py
@@ -0,0 +1,14 @@
+import time
+
+start = time.clock()
+
+seq_lines = [line.strip() for line in open('task2')]
+line = [i for i in range(1,10000000)]
+
+for seq in seq_lines:
+	s = ''.join([str(x) for x in line])
+	pos = s.find(str(seq)) + 1
+	print pos
+
+end = time.clock()
+print "%.2gs" % (end-start)
