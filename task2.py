80d6a5eae0e7b4f04a8d8b5114c792a4f548cab5
Ilya <mail@igusev.ru>
2014-09-04 19:09:59 +0400

    rm time tests

--- task2.py
+++ task2.py
@@ -1,7 +1,3 @@
-import time
-
-start = time.clock()
-
 seq_lines = [line.strip() for line in open('task2')]
 line = [i for i in range(1,10000000)]
 
@@ -9,6 +5,3 @@ for seq in seq_lines:
 	s = ''.join([str(x) for x in line])
 	pos = s.find(str(seq)) + 1
 	print pos
-
-end = time.clock()
-print "%.2gs" % (end-start)
