53e6aaedb409477ff56e5d9990f4d2d606fa7e1a
Ilya <mail@igusev.ru>
2014-09-04 23:31:59 +0400

    import values from file + some refactor

--- task2.py
+++ task2.py
@@ -1,7 +1,10 @@
-seq_lines = [line.strip() for line in open('task2')]
-line = [i for i in range(1,10000000)]
+#task2.py
+import sys
+
+seq_lines = [int(line) for line in open(sys.argv[1])]
 
 for seq in seq_lines:
-	s = ''.join([str(x) for x in line])
-	pos = s.find(str(seq)) + 1
-	print pos
+  line = [i for i in range(int(str(seq)[0]), seq)]
+  s = ''.join([str(x) for x in line])
+  pos = s.find(str(seq)) + 1
+  print pos
