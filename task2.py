f6ae2f88cc91bb8a766f06db63d25a2ef8a17e03
Ilya <mail@igusev.ru>
2014-09-04 23:37:56 +0400

    final task2

--- task2.py
+++ task2.py
@@ -2,9 +2,10 @@
 import sys
 
 seq_lines = [int(line) for line in open(sys.argv[1])]
+out = open(sys.argv[2], 'w')
 
 for seq in seq_lines:
   line = [i for i in range(int(str(seq)[0]), seq)]
   s = ''.join([str(x) for x in line])
   pos = s.find(str(seq)) + 1
-  print pos
+  out.write(str(pos)+'\n')
