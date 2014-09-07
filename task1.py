fbb5f3a716ae4cc2f74622bfec1f98964303adbf
Ilya <mail@igusev.ru>
2014-09-07 21:44:53 +0400

    write output into file

--- task1.py
+++ task1.py
@@ -28,7 +28,5 @@ for point in open(sys.argv[1]):
 
 #seq_lines = [int(i) for i in line.split() for line in open(sys.argv[1])]
 out = open(sys.argv[2], 'w')
-
-t = grahamscan(points);
-
-print t;
+for point in grahamscan(points):
+	out.write(str(point)+'\n')
